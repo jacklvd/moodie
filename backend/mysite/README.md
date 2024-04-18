2eb030e41eb595fe6d522fd0b803b4ff4fe2a564
Khoa Xuan Dang <dangkx@mail.uc.edu>
2024-04-17 23:56:34 -0400

    Fix Readme and add ferapp

--- backend/mysite/README.md
+++
@@ -1,7 +0,0 @@
-http://localhost:8000/account/register/
-    The API above help creating new info of user. It will ask usernaem, email, password, password2. 
-    Password2 is to check the user know their password( password == password2).
-http://localhost:8000/account/login/
-    This API ask user to type their username and password then return the authtoken.
-http://localhost:8000/account/logout/
-    This API logout the user account.
---
+++ backend/mysite/ferapp/views.py
@@ -0,0 +1,42 @@
+from .serializers import PostSerializer
+from .models import Post
+from rest_framework.views import APIView
+from rest_framework.parsers import JSONParser
+from rest_framework.response import Response
+from rest_framework import status
+import cv2
+import os
+import base64
+import numpy as np
+from fer import FER
+# Create your views here.
+
+
+class PostView(APIView):
+    parser_classes = [JSONParser]
+
+    def get(self, request, *args, **kwargs):
+        posts = Post.objects.all()
+        serializer = PostSerializer(posts, many=True)
+        return Response(serializer.data)
+
+
+class PostCreate(APIView):
+    parser_classes = [JSONParser]
+
+    def post(self, request, *args, **kwargs):
+        posts_serializer = PostSerializer(data=request.data)
+        if posts_serializer.is_valid():
+            posts_serializer.save()
+            encoded_string = posts_serializer.data['content'].split(',')[1]
+            decoded_data = base64.b64decode(encoded_string)
+            np_data = np.frombuffer(decoded_data, np.uint8)
+            img = cv2.imdecode(np_data, cv2.IMREAD_UNCHANGED)
+            output_image = cv2.imwrite(os.getcwd() + "test.jpg", img)
+            image_load = cv2.imread(os.getcwd() + "test.jpg")
+            detector = FER(mtcnn=True)
+            result = detector.top_emotion(image_load)
+            return Response(result[0], status=status.HTTP_201_CREATED)
+        else:
+            print('error', posts_serializer.errors)
+            return Response(posts_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
